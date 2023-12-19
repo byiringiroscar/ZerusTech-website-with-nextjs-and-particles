a20bdcc2c9ce89af03d31d494b2d28b0477fdef2
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-19 12:02:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Design from '../components/Design'
 export default function Home() {
   return (
     <div className='w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover text-white'>
-      <div>
+      <div className='w-full h-screen absolute top-0 left-0'>
         <Design />
       </div>
 
