61520a0194f1008c9cf55132d67fa431bead4912
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-19 11:18:09 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-banner-bg bg-no-repeat bg-center'>
+    <div className='w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover'>
 
     </div>
   )
