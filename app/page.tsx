6ad58a29d1f186ce91569b0a73f2a5c41add09b0
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-19 12:12:08 +0200

    Add banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Design from '../components/Design'
 import Header from '@/components/Header'
+import Banner from '@/components/Banner'
 
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover text-white'>
+    <div className='w-full h-auto bg-banner-bg bg-no-repeat bg-center bg-cover text-white'>
       <div className='max-w-6xl mx-auto text-white'>
         <Header />
+        <Banner />
       </div>
       <div className='w-full h-screen absolute top-0 left-0'>
         <Design />
