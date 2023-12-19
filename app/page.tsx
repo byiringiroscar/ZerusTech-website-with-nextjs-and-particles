5f01874b5b9a4022af5293372e371bdba0aca732
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-19 11:56:47 +0200

    Add particles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Design from '../components/Design'
 
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover'>
+    <div className='w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover text-white'>
+      <div>
+        <Design />
+      </div>
 
     </div>
   )
